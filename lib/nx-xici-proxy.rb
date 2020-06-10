0e4b05dbc77e9bda303dd84e31a98792a562948b
afeiship <1290657123@qq.com>
2020-06-10 18:00:52 +0800

    fix: require_nx path

--- lib/nx-xici-proxy.rb
+++ lib/nx-xici-proxy.rb
@@ -1,2 +1,2 @@
-require "nx/version"
-require "nx/xici-proxy"
+require_relative "./nx/version"
+require_relative "./nx/xici-proxy"
--- lib/nx/version.rb
+++ lib/nx/version.rb
@@ -1,5 +1,5 @@
 module Nx
   class XiciProxy
-    VERSION = "0.1.3"
+    VERSION = "0.1.4"
   end
 end
